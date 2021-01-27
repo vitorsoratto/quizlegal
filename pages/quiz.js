8827b114bbc4d34a66d118a54d0b1ee7a60ac7b1
Vitor Soratto <sorattoemail@gmail.com>
2021-01-26 22:24:24 -0300

    Added "!"

--- pages/quiz.js
+++ pages/quiz.js
@@ -40,6 +40,7 @@ export default function QuizPage() {
               Bem-vindo ao quiz,
               {' '}
               {uName}
+              !
             </h1>
           </Widget.Header>
           <Widget.Content>
